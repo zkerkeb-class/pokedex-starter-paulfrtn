b2b0dfb0921e895222a339dbd230566f6ce580cf
paulfrtn <paulalex.frtn@gmail.com>
2025-02-03 16:19:05 +0100

    First implementation of the card

---
+++ src/components/pokemonCard/PokemonCard.jsx
@@ -0,0 +1,14 @@
+import styles from './PokemonCard.module.css'
+
+const PokemonCard = () => {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.card}>
+                <p>Salut</p>
+            </div>
+        </div>
+    );
+}
+
+export default PokemonCard;
