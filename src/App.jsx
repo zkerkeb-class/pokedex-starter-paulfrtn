f110b88fd2a875d8b441e14f185207f056fdb458
paulfrtn <paulalex.frtn@gmail.com>
2025-02-17 09:37:03 +0100

    Added and refactored search bar and filtered selection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import pokemonsWithImages from "./assets/pokemons";
 import PokemonGrid from "./components/PokemonGrid/PokemonGrid.jsx";
 import { useState } from 'react';
 import SearchPokemon from "./components/SearchPokemon/SearchPokemon.jsx";
+import FilterPokemon from "./components/FilterPokemon/FilterPokemon.jsx";
 
 function App() {
     const [searchTerm, setSearchTerm] = useState('');
@@ -21,27 +22,7 @@ function App() {
                 <h1>Pokemon</h1>
                 <div>
                     <SearchPokemon searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                    <select onChange={(e) => setSelectedType(e.target.value)} style={{ padding: '8px' }}>
-                        <option value="">Tous les types</option>
-                        <option value="Bug">Bug</option>
-                        <option value="Dark">Dark</option>
-                        <option value="Dragon">Dragon</option>
-                        <option value="Electric">Electric</option>
-                        <option value="Fairy">Fairy</option>
-                        <option value="Fightning">Fighting</option>
-                        <option value="Fire">Fire</option>
-                        <option value="Flying">Flying</option>
-                        <option value="Ghost">Ghost</option>
-                        <option value="Grass">Grass</option>
-                        <option value="Ground">Ground</option>
-                        <option value="Ice">Ice</option>
-                        <option value="Normal">Normal</option>
-                        <option value="Poison">Poison</option>
-                        <option value="Psychic">Psychic</option>
-                        <option value="Rock">Rock</option>
-                        <option value="Steel">Steel</option>
-                        <option value="Water">Water</option>
-                    </select>
+                    <FilterPokemon selectedType={selectedType} setSelectedType={setSelectedType} />
                 </div>
             </header>
             <div className={"content"}>
